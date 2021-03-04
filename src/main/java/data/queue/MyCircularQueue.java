2dbf9a17ea844326ef50d934530ddf91e0dd21bd
haikuan <fujingjunben@gmail.com>
2021-03-04 18:36:40 +0800

    leetcode第一题

--- src/main/java/data/queue/MyCircularQueue.java
+++ src/main/java/data/queue/MyCircularQueue.java
@@ -1,3 +1,5 @@
+package data.queue;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -72,11 +74,11 @@ class MyCircularQueue {
     }
 
     public static void main(String[] args) {
-        MyCircularQueue circularQueue = new MyCircularQueue(3); // 设置长度为 3
-        circularQueue.enQueue(1); // 返回 true
+        MyCircularQueue circularQueue = new MyCircularQueue(3);
+        circularQueue.enQueue(1);
         circularQueue.enQueue(2); // 返回 true
         circularQueue.enQueue(3); // 返回 true
-        circularQueue.enQueue(4); // 返回 false，队列已满
+        circularQueue.enQueue(4);
         circularQueue.Rear(); // 返回 3
         circularQueue.isFull(); // 返回 true
         circularQueue.deQueue(); // 返回 true
