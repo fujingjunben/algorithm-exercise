ff308d09d6a4c60249a7cbd5b0fc79c2b89478cc
Haikuan Liu <fujingjunben@gmail.com>
2021-03-07 23:17:13 +0800

    算法

--- src/main/java/leetcode/s3/Solution.java
+++ src/main/java/leetcode/s3/Solution.java
@@ -1,38 +1,50 @@
 package leetcode.s3;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
         int maxLength = 0;
-        StringBuilder subString = new StringBuilder();
+        int left = 0;
+        Map<Character, Integer> keys = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            int position = subString.indexOf(String.valueOf(c));
-            if (position != -1) {
-                if (subString.length() > maxLength){
-                    maxLength = subString.length();
-                }
-                subString = new StringBuilder(
-                        subString.substring(position + 1));
+            if (keys.containsKey(c)) {
+                maxLength = Math.max(maxLength, i - left);
+                left = Math.max(left, keys.get(c) + 1);
             }
-            subString.append(c);
+            keys.put(c, i);
         }
-        return Math.max(subString.length(), maxLength);
+        return Math.max(maxLength, s.length() - left);
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         String s = "abcabcbb";
         int result = solution.lengthOfLongestSubstring(s);
-        System.out.println("length: " + result);
+        System.out.println( s + " : " + result);
         s = "pwwkew";
         result = solution.lengthOfLongestSubstring(s);
-        System.out.println("length: " + result);
+        System.out.println( s + " : " + result);
         s = "bbbbbb";
         result = solution.lengthOfLongestSubstring(s);
-        System.out.println("length: " + result);
+        System.out.println( s + " : " + result);
         s = "asjrgapa";
         result = solution.lengthOfLongestSubstring(s);
-        System.out.println("length: " + result);
+        System.out.println( s + " : " + result);
+        s = "aab";
+        result = solution.lengthOfLongestSubstring(s);
+        System.out.println( s + " : " + result);
+        s = "abba";
+        result = solution.lengthOfLongestSubstring(s);
+        System.out.println( s + " : " + result);
+        s = "dvdf";
+        result = solution.lengthOfLongestSubstring(s);
+        System.out.println( s + " : " + result);
+        s = "abcde";
+        result = solution.lengthOfLongestSubstring(s);
+        System.out.println( s + " : " + result);
 
     }
 }
