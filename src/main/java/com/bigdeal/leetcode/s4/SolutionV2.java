7f5fa55735e5a83a1b143b14f05fcb50bf1d361d
haikuan <fujingjunben@gmail.com>
2021-03-11 19:12:19 +0800

    计算中位数的另一种方法

--- src/main/java/com/bigdeal/leetcode/s4/SolutionV2.java
+++ src/main/java/com/bigdeal/leetcode/s4/SolutionV2.java
@@ -2,124 +2,41 @@ package com.bigdeal.leetcode.s4;
 
 public class SolutionV2 {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        Position p1 = shrink(nums1, 0, nums1.length - 1, 0, 0);
-        Position p2 = shrink(nums2, 0, nums1.length - 1, 0, 0);
-        int[] a = pickSmaller(nums1, nums2, p1.start, p1.end, p2.start, p2.end);
-        return getMedian(a, 0, a.length - 1);
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int k = (m + n + 1) / 2;
+        return splitArray(nums1, nums2, 0, 0, k);
     }
 
-    private int[] pickSmaller(int[] nums1, int[] nums2, int start1, int end1, int start2, int end2) {
-        int i = start1;
-        int j = start2;
-        int k = 0;
-        int[] nums3 = new int[end1 - start1 + end2 - start2 + 2];
-
-        while (i <= end1 && j <= end2 ) {
-            if (nums1[i] < nums2[j]) {
-                nums3[k++] = nums1[i++];
-            } else {
-                nums3[k++] = nums2[j++];
-            }
+    private double splitArray(int[] array1, int[] array2, int start1, int start2, int k) {
+        if (start1 >= array1.length) {
+            return getMedian(array2, start2, array2.length - 1);
         }
-        while (i <= end1) {
-            nums3[k++] = nums1[i++];
+        if (start2 >= array2.length) {
+            return getMedian(array1, start1, array1.length - 1);
         }
 
-        while (j <= end2) {
-            nums3[k++] = nums2[j++];
+        int smallOffset = k / 2 - 1;
+        int largeOffset = k - smallOffset;
+        if (array1[start1 + offset] <= array2[start2 + ]) {
+            return splitArray(array1, array2, start1 + offset, start2, k - Math.min());
+        } else {
+            return splitArray(array1, array2, start1, start2 + k - (k / 2 - 1), k - (k / 2 - 1));
         }
-        return nums3;
     }
 
-    private Position shrink(int[] nums, int start, int end, double low, double high){
-        if ((end - start) <= 2){
-            return new Position(start, end);
-        }
-
-        int nextStart = start;
-        int nextEnd = end;
-        if (nums[start] < low) {
-            nextStart = (end + start) / 2 + 1;
-        }
-        if (nums[end] > high) {
-            nextEnd = (end - nextStart) / 2;
-        }
-
-        double median = getMedian(nums, nextStart, nextEnd);
-
-        return shrink(nums, nextStart, nextEnd, Math.max(low, median), Math.min(high, median));
-    }
-
-    private double getMedian(int[] nums, int start, int end){
+    double getMedian(int[] nums, int start, int end){
         if (start == end) {
             return nums[start];
         }
         int size = end - start + 1;
         double median = 0;
         if (size % 2 == 0) {
-            median = (nums[size / 2] + nums[size / 2 - 1]) / 2.0;
+            median = (nums[start + size / 2] + nums[start + size / 2 - 1]) / 2.0;
         } else {
-            median = nums[(size - 1) / 2];
+            median = nums[start + (size - 1) / 2];
         }
         return median;
-    }
-
-    private static class Position {
-        public int start;
-        public int end;
-
-        public Position(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
-
-        public int size(){
-            return this.end - this.start + 1;
-        }
-    }
-
-    public static void main(String[] args) {
-        SolutionV2 solution = new SolutionV2();
-        int[] nums1 = new int[]{1, 3, 5, 6, 10};
-        int[] nums2 = new int[]{7, 8, 9, 12, 20};
-
-        if (solution.findMedianSortedArrays(nums1, nums2) == (7 + 8) / 2.0){
-            System.out.println("1. correct");
-        } else {
-            System.out.println("1. false");
-        }
-        int[] nums3 = new int[]{1, 3, 5, 6, 10};
-        int[] nums4 = new int[]{2, 4, 9, 12, 20};
-
-        if (solution.findMedianSortedArrays(nums3, nums4) == (5 + 6) / 2.0){
-            System.out.println("2. correct");
-        } else {
-            System.out.println("2. false");
-        }
-        int[] nums5 = new int[]{1, 3, 5, 6};
-        int[] nums6 = new int[]{2, 4, 9, 12, 20};
-
-        if (solution.findMedianSortedArrays(nums5, nums6) == 5.0){
-            System.out.println("3. correct");
-        } else {
-            System.out.println("3. false");
-        }
-        int[] nums7 = new int[]{2, 5, 20, 30, 31, 42, 52, 62};
-        int[] nums8 = new int[]{4, 8, 10 ,34, 40, 63, 78};
-        int[] test = new int[]{2, 4, 5, 8, 10, 20, 30, 31, 34, 40, 42, 52, 62, 63, 78};
-        double median = solution.getMedian(nums7, 0, nums7.length - 1);
-        System.out.println("nums7 median: " + median);
-        median = solution.getMedian(nums8,0, nums8.length - 1);
-        System.out.println("nums8 median: " + median);
-        median = solution.getMedian(test, 0, test.length - 1);
-        System.out.println("test median: " + median);
-
-        if (solution.findMedianSortedArrays(nums7, nums8) == 31){
-            System.out.println("4. correct");
-        } else {
-            System.out.println("4. false");
-        }
-
-
     }
 }
---
+++ src/main/java/com/bigdeal/leetcode/s4/Test.java
@@ -0,0 +1,46 @@
+package com.bigdeal.leetcode.s4;
+
+public class Test {
+    public static void main(String[] args) {
+        SolutionV2 solution = new SolutionV2();
+        int[] nums1 = new int[]{1, 3, 5, 6, 10};
+        int[] nums2 = new int[]{7, 8, 9, 12, 20};
+
+        if (solution.findMedianSortedArrays(nums1, nums2) == (7 + 8) / 2.0){
+            System.out.println("1. correct");
+        } else {
+            System.out.println("1. false");
+        }
+        int[] nums3 = new int[]{1, 3, 5, 6, 10};
+        int[] nums4 = new int[]{2, 4, 9, 12, 20};
+
+        if (solution.findMedianSortedArrays(nums3, nums4) == (5 + 6) / 2.0){
+            System.out.println("2. correct");
+        } else {
+            System.out.println("2. false");
+        }
+        int[] nums5 = new int[]{1, 3, 5, 6};
+        int[] nums6 = new int[]{2, 4, 9, 12, 20};
+
+        if (solution.findMedianSortedArrays(nums5, nums6) == 5.0){
+            System.out.println("3. correct");
+        } else {
+            System.out.println("3. false");
+        }
+        int[] nums7 = new int[]{2, 5, 20, 30, 31, 42, 52, 62};
+        int[] nums8 = new int[]{4, 8, 10 ,34, 40, 63, 78};
+        int[] test = new int[]{2, 4, 5, 8, 10, 20, 30, 31, 34, 40, 42, 52, 62, 63, 78};
+        double median = solution.getMedian(nums7, 0, nums7.length - 1);
+        System.out.println("nums7 median: " + median);
+        median = solution.getMedian(nums8,0, nums8.length - 1);
+        System.out.println("nums8 median: " + median);
+        median = solution.getMedian(test, 0, test.length - 1);
+        System.out.println("test median: " + median);
+
+        if (solution.findMedianSortedArrays(nums7, nums8) == 31){
+            System.out.println("4. correct");
+        } else {
+            System.out.println("4. false");
+        }
+    }
+}
