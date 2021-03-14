c66349f45fb0cd282d8610e2faac73f01679dd99
haikuan <fujingjunben@gmail.com>
2021-03-14 23:30:41 +0800

    去掉数组中的重复元素

--- src/main/java/com/bigdeal/leetbook/simple/s1/Solution.java
+++ src/main/java/com/bigdeal/leetbook/simple/s1/Solution.java
@@ -6,13 +6,12 @@ import java.util.Map;
 public class Solution {
     public int removeDuplicates(int[] nums) {
         int size = 0;
-        int offset = 0;
-        for (int i = 0; i < nums.length; i = i + offset) {
-            if (i > 1) {
-                if (nums[i - 1] == nums[i]) {
-                    offset++;
-                } else if (offset != 0) {
-                    nums[i - offset] = nums[i];
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0) {
+                if (nums[i - 1] != nums[i]) {
+                    if (size != i) {
+                        nums[size] = nums[i];
+                    }
                     size++;
                 }
             } else {
---
+++ src/main/java/com/bigdeal/leetbook/simple/s1/Test.java
@@ -0,0 +1,21 @@
+package com.bigdeal.leetbook.simple.s1;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] a1 = new int[]{1, 1, 2};
+        int size = solution.removeDuplicates(a1);
+        System.out.println("a1: " + size);
+        printArray(a1, size);
+        a1 = new int[]{0,0,1,1,1,2,2,3,3,4};
+        size = solution.removeDuplicates(a1);
+        System.out.println("a2: " + size);
+        printArray(a1, size);
+    }
+
+    public static void printArray(int[] num, int len) {
+        for (int i = 0; i < len; i++) {
+            System.out.println(num[i]);
+        }
+    }
+}
