4bc0552bb72108e0b7cd02ddade99665ee051e6d
lhk <fujingjunben@gmail.com>
2021-05-29 10:49:57 +0800

    delete nth node in reverse

--- src/main/java/com/bigdeal/leetbook/simple/linklist/s2/Solution.java
+++ src/main/java/com/bigdeal/leetbook/simple/linklist/s2/Solution.java
@@ -1,4 +1,36 @@
 package com.bigdeal.leetbook.simple.linklist.s2;
 
+import com.bigdeal.leetbook.simple.linklist.ListNode;
+
 public class Solution {
+    public ListNode deleteNode(ListNode head, int n) {
+        return deleteNodeByPointer(head, n);
+    }
+
+    private ListNode deleteNodeByPointer(ListNode head, int n) {
+        ListNode right = head;
+        ListNode left = head;
+
+        for (int i = 0; i < n; i++) {
+            if (right.next != null) {
+                right = right.next;
+            }
+        }
+
+        if (right == null) {
+            return head;
+        }
+
+        if (head.next == right) {
+            return right;
+        }
+
+        while (right.next != null) {
+            right = right.next;
+            left = left.next;
+        }
+        left.next = left.next.next;
+
+        return head;
+    }
 }
