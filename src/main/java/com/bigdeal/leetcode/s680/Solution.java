ae296ea92b612f5cbfe6b48c4779a43f6ced86de
lhk <fujingjunben@gmail.com>
2021-10-31 18:09:41 +0800

    使用递归+双指针解决680

--- src/main/java/com/bigdeal/leetcode/s680/Solution.java
+++ src/main/java/com/bigdeal/leetcode/s680/Solution.java
@@ -1,4 +1,23 @@
 package com.bigdeal.leetcode.s680;
 
 public class Solution {
+    public boolean validPalindrome(String s) {
+        return helper(s, 0, s.length() - 1, 0);
+    }
+
+    private boolean helper(String s, int left, int right, int count) {
+        if (count > 1) {
+            return false;
+        }
+        while (left < right) {
+            if (s.charAt(left) == s.charAt(right)) {
+                left++;
+                right--;
+            } else {
+                return helper(s, left + 1, right, count + 1)
+                        || helper(s, left, right - 1, count + 1);
+            }
+        }
+        return true;
+    }
 }
--- src/test/java/com/bigdeal/leetcode/s680/TestSolution.java
+++ src/test/java/com/bigdeal/leetcode/s680/TestSolution.java
@@ -1,4 +1,55 @@
 package com.bigdeal.leetcode.s680;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TestSolution {
+    @Test
+    public void test1() {
+        String s = "aba";
+        Solution solution = new Solution();
+        boolean result = solution.validPalindrome(s);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void test2() {
+        String s = "abca";
+        Solution solution = new Solution();
+        boolean result = solution.validPalindrome(s);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void test3() {
+        String s = "abc";
+        Solution solution = new Solution();
+        boolean result = solution.validPalindrome(s);
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void test4(){
+        String s = "aabdeenddbaagbddeedbaa";
+        Solution solution = new Solution();
+        boolean result = solution.validPalindrome(s);
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void test5(){
+        String s = "aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga";
+        Solution solution = new Solution();
+        boolean result = solution.validPalindrome(s);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void test6(){
+        String s = "acufxfucua";
+        Solution solution = new Solution();
+        boolean result = solution.validPalindrome(s);
+        Assertions.assertTrue(result);
+    }
+
 }
