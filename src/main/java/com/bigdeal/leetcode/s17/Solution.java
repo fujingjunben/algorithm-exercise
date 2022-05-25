364aaf80a11bfb8cebc1e94294b7f440164870a7
LHK <fujingjunben@gmail.com>
2022-05-25 23:13:01 +0800

    leetcode 17题

---
+++ src/main/java/com/bigdeal/leetcode/s17/Solution.java
@@ -0,0 +1,13 @@
+package com.bigdeal.leetcode.s17;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        HashMap<String, List<String>> table = new HashMap<>();
+        table.put("2", new ArrayList<String>(){})
+    }
+
+}
